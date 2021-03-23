88b5b167a2fd457bae998829addafb062829959c
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-03-22 18:18:34 -0700

    finish lab6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import axios from "axios";
 
-import Button from 'react-bootstrap/Button';
-// import Form from "react-bootstrap/Form";
-// import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+
+import backgroundImg from "/Users/Yongjoo/Desktop/codefellows/city/src/img/bg_img.jpeg";
 
 class App extends React.Component {
   constructor(props) {
@@ -16,42 +20,74 @@ class App extends React.Component {
       imgSrc: "",
       displayResults: false,
     };
-    // console.log('constructor');
   }
 
   getLocationInfo = async (e) => {
-    e.preventDefault();
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
-    const location = await axios.get(url);
-    const locationArray = location.data;
-    this.setState({
-      location: locationArray[0],
-      displayResults: true,
-      imgSrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13`,
-    });
+    try {
+      e.preventDefault();
+      const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+      const location = await axios.get(url);
+      const locationArray = location.data;
+      this.setState({
+        location: locationArray[0],
+        displayResults: true,
+        imgSrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=12`,
+      });
+    } catch (err) {
+      if (!alert(err)) {
+        window.location.reload();
+      }
+    }
   };
-
   render() {
+    var divStyle = {
+      backgroundImage: `url(${backgroundImg})`,
+      height: "100vh",
+      minHeight: "100vh",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+    };
     console.log("state", this.state);
     return (
-      <>
-        <h1>welcome city explorer!</h1>
-        <Button variant="dark">Click</Button>
-        <form onSubmit={this.getLocationInfo}>
-          <input
-            onChange={(e) => this.setState({ searchQuery: e.target.value })}
-            placeholder="city"
-          />
-          <Button variant="primary">explore!</Button>
-        </form>
-
-        {this.state.displayResults && (
-          <>
-            <h2>{this.state.location.display_name}</h2>
-            <img src={this.state.imgSrc} alt="map" />
-          </>
-        )}
-      </>
+      <div style={divStyle}>
+        <Container className="align-middle p-0">
+          <h1 style={{ color: "white", paddingTop: "5rem" }}>
+            welcome city explorer!
+          </h1>
+          <Form onSubmit={this.getLocationInfo} style={{ marginTop: "2rem" }}>
+            <Form.Row>
+              <input
+                onChange={(e) => this.setState({ searchQuery: e.target.value })}
+                placeholder="Enter a location!"
+              />
+              <Col>
+                <Button variant="primary" type="submit">
+                  explore!
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+          <div style={{ marginTop: "2rem" }}>
+            {this.state.displayResults && (
+              <>
+                <h2 style={{ color: "white" }}>
+                  {this.state.location.display_name}
+                </h2>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                  <Image
+                    fluid
+                    thumbnail
+                    src={this.state.imgSrc}
+                    alt="map"
+                    style={{ alignSelf: "center" }}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+        </Container>
+      </div>
     );
   }
 }
