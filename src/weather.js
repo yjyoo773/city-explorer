23f8ae40123f46725d77804be845738a38ffe492
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-03-23 17:23:53 -0700

    finish lab7

---
+++ src/weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CardDeck from "react-bootstrap/CardDeck";
+import WeatherDay from "./weatherday";
+
+class Weather extends React.Component {
+  render() {
+    let data = this.props.weather;
+    return (
+      <CardDeck style={{alignSelf:"center"}}>
+        {data.map((element) => (
+          <WeatherDay key={element.date} date={element.date} desc={element.description} />
+        ))}
+      </CardDeck>
+    );
+  }
+}
+export default Weather;
