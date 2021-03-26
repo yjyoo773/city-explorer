2ab0a04f74b67a58bc16c9b9ff418da32b378dab
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-03-26 16:23:27 -0700

    update according to server code

--- src/movie.js
+++ src/movie.js
@@ -1,17 +1,29 @@
 import React from "react";
 // import MovieC from "./movie_child";
 import Carousel from "react-bootstrap/Carousel";
+import Image from "react-bootstrap/Image";
 
 class Movie extends React.Component {
+  addDefaultSrc(e) {
+    e.target.src = "http://via.placeholder.com/300x450";
+  }
   render() {
     let data = this.props.movie;
 
     // console.log(data);
     return (
-      <Carousel style={{ alignSelf: "center",minHeight:"8rem" }}>
-        {data.map((element) => (
-          <Carousel.Item>
-            <img style={{alignSelf: "center",minHeight:'400px',color:'white'}} src={element.image_url} alt={element.title} />
+      <Carousel style={{ alignSelf: "center", minHeight: "8rem" }}>
+        {data.map((element, indx) => (
+          <Carousel.Item key={indx}>
+            <Image
+              style={{
+                alignSelf: "center",
+                minHeight: "400px",
+              }}
+              src={element.image_url}
+              alt={element.title}
+              onError={this.addDefaultSrc}
+            />
             <Carousel.Caption>
               {/* <h3>{element.title}</h3> */}
               <h4>{element.released_on}</h4>
@@ -25,7 +37,6 @@ class Movie extends React.Component {
           // />
         ))}
       </Carousel>
-      
     );
   }
 }
